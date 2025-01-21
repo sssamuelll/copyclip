5bb169ecabafd589fdbac6a2050e8b527fb9164b
sssamuelll <sssamuelll@gmail.com>
2025-01-21 16:25:04 +0100

    Added setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="copyclip",
+    version="1.0.0",
+    author="Samuel Dario",
+    author_email="correo@samueldar.io",
+    description="Copy file contents from a directory to the clipboard.",
+    packages=find_packages(),
+    py_modules=["copyclip"],  # Nombre del script sin extensión
+    install_requires=[
+        "pyperclip",
+        "tqdm",
+        "gitignore-parser"
+    ],
+    entry_points={
+        "console_scripts": [
+            "copyclip=copyclip:main",  # El nombre del comando y la función principal
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
